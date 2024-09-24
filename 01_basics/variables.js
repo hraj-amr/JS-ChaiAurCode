9303668cf4126c600f7b1e362ddd2864bbc2213d
hraj-amr <hraj.amr@gmail.com>
2024-09-24 03:01:26 +0000

    done

---
+++ 01_basics/variables.js
@@ -0,0 +1,14 @@
+const accountId = 274536
+let accountEmail = "Harshit@gmail.com"
+var accountPassword = "12345"
+accountCity = "Gaya"
+let accountState;
+
+// accountId = 23456 // not allowed
+
+accountEmail="amr@gmail.com"
+accountPaaword = "754689"
+accountCity = "Patna"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
