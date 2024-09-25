a42b0eb9dd1643ffa6d686d52f94923713652f88
hraj-amr <hraj.amr@gmail.com>
2024-09-25 03:08:02 +0000

    datatypes done

---
+++ 01_basics/03_datatypes.js
@@ -0,0 +1,37 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 +3)  //we are using nodejs, not browser
+
+// console.log(3+4); console.log("harshit")  //use semicolon in same line   // Bad readability
+
+// console.log( 3 + 
+//             4 )  // code readablity should be high
+
+// console.log(3+4)
+// console.log("harshit")
+
+
+let name = "Harshit"            // string
+let age = 19                    // number
+let isLoggedIn = false          // boolean
+let state = null;               // null (empty)
+let city;                       // undefined
+
+
+//Primitive Datatypes
+
+// number => 2 to power 53
+// bigint
+// string => " "
+// boolean => true/false
+// null => standalone value
+// undefined => variable declared but value not assigned yet
+// symbol => unique
+
+
+// Object
+
+console.log(typeof "Harshit");      // string
+console.log(typeof age);            // number
+console.log(typeof null);           // Object
+console.log(typeof undefined);      // undefined
