4f7e193b1e9088f816c29559f0cc68b3624941ef
hraj-amr <hraj.amr@gmail.com>
2024-09-28 12:35:33 +0000

    comparisons done

---
+++ 01_basics/05_comparisons.js
@@ -0,0 +1,26 @@
+console.log(2>1);
+console.log(2<1);
+console.log(2>=1);
+console.log(2<=1);
+console.log(2!=1);
+
+// Different datatypes
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+// Null
+
+console.log(null == 0);
+console.log(null > 0);
+console.log(null >= 0);
+
+// undefined
+
+console.log(undefined == 0);
+console.log(undefined < 0);
+console.log(undefined > 0);
+
+// strict equality check (===)
+
+console.log("20" === 20);
