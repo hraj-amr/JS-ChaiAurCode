9f4b1c8f923ba7fc5a1f024b32ea316dc73f84c1
hraj-amr <hraj.amr@gmail.com>
2024-09-29 23:50:49 +0000

    Stack and Heap

---
+++ 01_basics/07_StackHeap.js
@@ -0,0 +1,18 @@
+let name = "harshit"
+let anothername = name
+anothername = "amar"
+
+console.log(name);
+console.log(anothername);
+
+
+let user = {
+    id: 321,
+    name: "abc"
+}
+
+let user2 = user
+user2.id = 8910
+
+console.log(user.id);
+console.log(user2.id);
