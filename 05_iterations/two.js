ce9e5a603c12d82952fcc807121c82edc87a9e26
hraj-amr <hraj.amr@gmail.com>
2024-11-16 18:49:42 +0000

    day 26

---
+++ 05_iterations/two.js
@@ -0,0 +1,42 @@
+// while loop
+
+// syntax
+// while (condition) {
+    
+// }
+
+
+// simple loop
+let index = 1
+while (index <= 10) {
+    console.log(`Value of index is ${index}`);
+    index = index + 2
+}
+
+
+// array
+let myArray = ['flash', 'batman', 'superman']
+
+let arr = 0
+while(arr<myArray.length){
+    console.log(`Value is ${myArray[arr]}`);
+    arr = arr + 1
+}
+
+
+
+// do while loop
+
+// syntax
+do {
+    
+} while (condition);
+
+
+// simple loop
+let score = 1
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);          // execute pahle hoga, condition baad me check hoga
+
